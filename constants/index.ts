5e78228ba15e469a1416ac899ad90be3c273cd15
Filipe Ramos <filipe.ramos8@gmail.com>
2024-10-22 22:44:45 +0200

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,168 @@
+export const Skill_data = [
+  {
+    skill_name: "Pandas",
+    Image: "/pandas.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    Image: "/chromadb.png",
+    width: 80,
+    height: 80,
+    name: "ChromaDB",
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+
+export const Frontend_skill = [
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Java Script",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Type Script",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js 14",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Backend_skill = [
+  {
+    skill_name: "My SQL",
+    Image: "/mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    Image: "/graphql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    Image: "/flask-logo.png",
+    width: 100,
+    height: 100,
+    name: "Flask",
+  },
+  {
+    Image: "/js.png",
+    width: 70,
+    height: 70,
+    name: "JavaScript",
+  },
+];
+
+export const Full_stack = [
+  {
+    skill_name: "Docker",
+    Image: "/docker.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Kubernetes",
+    Image: "/kubernetes.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Figma",
+    Image: "/figma.png",
+    width: 50,
+    height: 50,
+  },
+];
+
+export const Other_skill = [
+  {
+    Image: "/c.png",
+    width: 80,
+    height: 80,
+    name: "C",
+  },
+  {
+    Image: "/cpp.png",
+    width: 80,
+    height: 80,
+    name: "C++",
+  },
+  {
+    Image: "/java.png",
+    width: 80,
+    height: 80,
+    name: "Java",
+  },
+  {
+    Image: "/scala.png",
+    width: 80,
+    height: 80,
+    name: "Scala",
+  },
+  {
+    Image: "/chromadb.png",
+    width: 80,
+    height: 80,
+    name: "ChromaDB",
+  },
+  {
+    Image: "/scikit-learn.png",
+    width: 100,
+    height: 100,
+    name: "Scikit-learn",
+  },
+  {
+    Image: "/sqlalchemy-logo.png",
+    width: 100,
+    height: 100,
+    name: "SQLAlchemy",
+  },
+  {
+    Image: "/python.png",
+    width: 80,
+    height: 80,
+    name: "Python",
+  },
+];
+
