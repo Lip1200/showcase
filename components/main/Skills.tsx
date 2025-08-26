c479c5e5a5349a27621eee224e48ee1a4f68436a
Filipe Ramos <97791523+Lip1200@users.noreply.github.com>
2025-08-26 09:31:01 +0200

    refactor: use tailwind scale classes for skills

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -21,8 +21,7 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-20 py-10"
-      style={{ transform: "scale(0.9)" }}
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-20 py-10 scale-90 md:scale-100"
     >
       <SkillText />
 
